79af297db83dce883e8f61c8a04f891c3fcacac0
seniorWebDeveloperLife <61110993+seniorWebDeveloperLife@users.noreply.github.com>
2020-02-17 10:23:47 -0500

    adding header for impactRadius.com
    adding header for impactRadius.com, fixing some spacing issues and only using one document ready function.

--- tocAndImageThumbnailer.js
+++ tocAndImageThumbnailer.js
@@ -1,5 +1,9 @@
-$(document).ready(function(){
 
+
+/*
+**** 	image thumbnailer functions
+*/
+	
   // set the target width
   var smallWidth = 100;
 // get a string width to use in the css
@@ -16,20 +20,33 @@ function toggleWidth(image){
    sWidth = getWidth(image[0].width);
    $(image[0]).attr('width', sWidth);	
 }
+	
+	
 $(document).ready(function(){
-// set all images small
-$('img.img-responsive').attr('width', '100px');
-// bind to click
-$('img.img-responsive').on('click', function(){
+
+	/*
+	**** 	adding header for impactRadius.com
+	*/
+	 $("head").append("<meta name='ir-site-verification-token' value='-1570783606' />");
+	
+	/*
+	**** 	image thumbnailer 
+	*/
+	// set all images small
+	$('img.img-responsive').attr('width', '100px');
+	// bind to click
+	$('img.img-responsive').on('click', function(){
 	toggleWidth($(this));
-});
+	});
 
-});
-
-    $('div.post-content :header').each(function( index ) {
-        var slugText = $( this ).text().replace(/\s/g, '');
-	var linkText =  "<br><a href='#" + slugText + "' onclick=\"setTimeout(function(){ $('html, body').animate({scrollTop: '-=85px'}, 100)}, 100);\">" +$( this ).text() + "</a>" ;
-        $( "#tableOfContents" ).append( linkText);
-        $( this ).before('<a name="' + slugText + '" href="#top">(top)</a><br>');
-    });
+	/*
+	**** 	Table Of Contents Generator
+	*/
+	
+    	$('div.post-content :header').each(function( index ) {
+       	 	var slugText = $( this ).text().replace(/\s/g, '');
+		var linkText =  "<br><a href='#" + slugText + "' onclick=\"setTimeout(function(){ $('html, body').animate({scrollTop: '-=85px'}, 100)}, 100);\">" +$( this ).text() + "</a>" ;
+        	$( "#tableOfContents" ).append( linkText);
+        	$( this ).before('<a name="' + slugText + '" href="#top">(top)</a><br>');
+    	});
 });
